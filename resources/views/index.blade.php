3b1898a3a21af85cf054ed7e5797633a8f9ebda3
adriswara <chevellerz454@yahoo.com>
2025-06-19 09:29:20 +0700

    database setup, index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!-- resources/views/my-page.blade.php -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Book Rating Application</title>
+</head>
+
+<body>
+    <h1>Book Rating Application</h1>
+    <p>This is a new page created in Laravel.</p>
+
+    
+
+</body>
+
+</html>
